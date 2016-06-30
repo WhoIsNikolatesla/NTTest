3990d323452c3af87254d21a1ac33ebe5d079bbc
刘明 <uaremyonlygril@gmail.com>
2016-06-30 21:33:37 +0800

    创建项目

---
+++ NTTest/NTTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NTTest
+//
+//  Created by chenzhaoliu on 16/6/30.
+//  Copyright © 2016年 chenzhaoliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NTTest/NTTestTests/NTTestTests.m
@@ -0,0 +1,39 @@
+//
+//  NTTestTests.m
+//  NTTestTests
+//
+//  Created by chenzhaoliu on 16/6/30.
+//  Copyright © 2016年 chenzhaoliu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NTTestTests : XCTestCase
+
+@end
+
+@implementation NTTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
